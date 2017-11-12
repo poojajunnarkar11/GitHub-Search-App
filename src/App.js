252532c64cf3ab4c6e6110f82d86106f60c8b8c5
Pooja Junnarkar <pooja.junnarkar@copart.com>
2017-11-11 20:44:38 -0600

    searching from input

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react'
 import Form from './Form'
 import { CardList } from './CardList'
-import { UserList } from './constants/UserList'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      userList: [
+        {
+          avatar_url: 'https://avatars2.githubusercontent.com/u/15823037?v=4',
+          name: 'Pooja Junnarkar',
+          company: 'Copart'
+        }
+      ]
+    }
+  }
+  findNewUsers = newUsers => {
+    this.setState(prevState => ({
+      userList: prevState.userList.concat(newUsers)
+    }))
+  }
   render() {
     return (
       <div>
-        <Form />
-        <CardList userList={UserList} />
+        <Form submitReferenceFunc={this.findNewUsers} />
+        <CardList userList={this.state.userList} />
       </div>
     )
   }
--- src/Form.js
+++ src/Form.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class Form extends Component {
+  constructor() {
+    super()
+    this.state = {
+      userName: ''
+    }
+  }
+  handleSubmit = event => {
+    event.preventDefault()
+    axios.get(`https://api.github.com/users/${this.state.userName}`).then(response => {
+      this.props.submitReferenceFunc(response.data)
+    })
+  }
   render() {
     return (
-      <div style={{ margin: '1em' }}>
-        <input type="text" placeholder="GitHub username" />
+      <form onSubmit={this.handleSubmit} style={{ margin: '1em' }}>
+        <input
+          type="text"
+          value={this.state.userName}
+          onChange={event => this.setState({ userName: event.target.value })}
+          placeholder="GitHub username"
+        />
         <button type="submit">Find User</button>
-      </div>
+      </form>
     )
   }
 }
